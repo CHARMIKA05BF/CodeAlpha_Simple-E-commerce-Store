fa2e20c97913b26288e4607dd2264c308ded2380
Bella Gnana Charmika <gnanachamikabella@gmail.com>
2026-06-26 12:48:42 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(simple-ecommerce-store, "../frontend/index.html"));
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
